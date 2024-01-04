aad979b195396deb28be1a45339e84904fbb799b
김종민 <jm.kim@lifesemantics.kr>
2024-01-04 17:59:49 +0900

    백업

--- app/src/main/java/com/example/smaplenotification/MainActivity.kt
+++ app/src/main/java/com/example/smaplenotification/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.smaplenotification
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.example.smaplenotification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,24 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        CallService().startService(this)
 
         binding.btnIncomingCall.setOnClickListener {
-            startNotification(1)
+            CallService().notification1(this)
         }
 
         binding.btnOngoingCall.setOnClickListener {
-            startNotification(2)
+            CallService().notification2(this)
         }
 
         binding.btnScreeningCall.setOnClickListener {
-            startNotification(3)
+            CallService().notification3(this)
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.P)
-    private fun startNotification(notificationId: Int) {
-        val serviceIntent = Intent(this, CallService::class.java)
-        serviceIntent.putExtra("NOTIFICATION_ID", notificationId)
-        ContextCompat.startForegroundService(this, serviceIntent)
     }
 }
