8e712343a1ec44ab418cb67103e987cbc1de1fce
김종민 <jm.kim@lifesemantics.kr>
2024-01-23 10:24:35 +0900

    SDK 34 대응

--- app/src/main/java/com/example/smaplenotification/MainActivity.kt
+++ app/src/main/java/com/example/smaplenotification/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.smaplenotification
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.smaplenotification.databinding.ActivityMainBinding
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestPermission(this)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         CallService().startService(this)
@@ -29,4 +35,26 @@ class MainActivity : AppCompatActivity() {
             CallService().notification3(this)
         }
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+private fun requestPermission(context: Context) {
+    TedPermission.create()
+        .setPermissionListener(object : PermissionListener {
+            override fun onPermissionGranted() {
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {
+                Toast.makeText(
+                    context,
+                    "permission request",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+        .setDeniedMessage("Please grant permission. [Settings] > [Apps & notifications] > [Advanced] > [App permissions]")
+        .setPermissions(
+            android.Manifest.permission.POST_NOTIFICATIONS,
+        )
+        .check()
 }
